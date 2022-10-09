d1f4e269dddbd1540c666d2320d5e2f1ec578645
jay <jayshah5109@gmail.com>
2022-10-09 15:39:35 +0530

    added /authentication endpoint controller

--- src/main/java/com/demo/springsecurity/HelloResource.java
+++ src/main/java/com/demo/springsecurity/HelloResource.java
@@ -1,13 +1,53 @@
 package com.demo.springsecurity;
 
+import com.demo.springsecurity.model.AuthenticationRequest;
+import com.demo.springsecurity.model.AuthenticationResponse;
+import com.demo.springsecurity.service.MyUserDetailsService;
+import com.demo.springsecurity.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloResource {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+    @Autowired
+    private JwtUtil jwtUtil;
 
     @RequestMapping("/hello")
     public String helloWord() {
         return "Hello World!";
     }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+
+        // Below try catch block authenticates username and password
+        try {
+            new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+        } catch (BadCredentialsException e) {
+            throw new Exception("Invalid username or password",e);
+        }
+
+        // If username and password are correct, then JWT token will be sent as seen below
+        final UserDetails userDetails = myUserDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
+
+
 }
--- src/main/java/com/demo/springsecurity/config/SecurityConfigurer.java
+++ src/main/java/com/demo/springsecurity/config/SecurityConfigurer.java
@@ -1,9 +1,10 @@
-package com.demo.springsecurity;
+package com.demo.springsecurity.config;
 
 import com.demo.springsecurity.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -18,6 +19,16 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(myUserDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/authenticate")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
     }
 
     @Bean
